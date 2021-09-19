665f828b0babf09372ae76471ef65dd6176bc4fc
DeadSage <79532594+DeadSage@users.noreply.github.com>
2021-09-19 17:25:06 +0300

    mysql + model/views for dynamic data upload

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'coursera_blog',
+        'USER': 'coursera_blog',
+        'PASSWORD': 'coursera_blog',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'OPTIONS': {'charset': 'utf8'}
     }
 }
 
